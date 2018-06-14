b44e6c9f7dda2e6b4181170df6ab29b85f576f3e
Will Pride <wpride@dimagi.com>
2018-06-14 15:53:29 -0400

    Add case property utils, package selector

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 Provides a set of utility functions for using CommCare Android [mobile APIs](https://github.com/dimagi/commcare-android/wiki)
+
+### Examples
+
+#### Case Name Lookup
+
+CaseUtils.getCaseName(Context context, String caseId);
--- app/src/main/java/org/commcare/commcaresupportlibrary/CaseUtils.java
+++ app/src/main/java/org/commcare/commcaresupportlibrary/CaseUtils.java
@@ -1,19 +1,112 @@
 package org.commcare.commcaresupportlibrary;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.net.Uri;
 
-import static org.commcare.commcaresupportlibrary.Constants.CASE_DB_ATTACHMENT_URI;
-import static org.commcare.commcaresupportlibrary.Constants.CASE_DB_INDEX_URI;
-import static org.commcare.commcaresupportlibrary.Constants.CASE_DB_LIST_URI;
-import static org.commcare.commcaresupportlibrary.Constants.CASE_DB_SINGLE_URI;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static android.content.Context.MODE_PRIVATE;
 
 /**
  * Created by willpride on 3/27/18.
  */
 
 public class CaseUtils {
+
+    private static String getPackageExtension(Context context) {
+        SharedPreferences prefs = context.getSharedPreferences("support-library", MODE_PRIVATE);
+        return prefs.getString("extension", null);
+    }
+
+    public static void setPackageExtension(Context context, String extension) {
+        SharedPreferences.Editor editor = context.getSharedPreferences("support-library", MODE_PRIVATE).edit();
+        editor.putString("extension", extension);
+        editor.apply();
+    }
+
+    private static String getCaseDbBaseUri(Context context) {
+        StringBuilder uriBuilder = new StringBuilder("content://");
+        String packageName = Constants.BASE_PACKAGE_NAME;
+        uriBuilder.append(packageName);
+        String packageExtension  = getPackageExtension(context);
+        if (packageExtension != null) {
+            uriBuilder.append(".").append(packageExtension);
+        }
+        uriBuilder.append(".case").append("/casedb/");
+        return uriBuilder.toString();
+    }
+
+    private static String getCaseDbListUri(Context context) {
+        return getCaseDbBaseUri(context) + "case/";
+    }
+
+    private static String getCaseDbDataUri(Context context) {
+        return getCaseDbBaseUri(context) + "data/";
+    }
+
+    private static String getCaseDbAttachmentUri(Context context) {
+        return getCaseDbBaseUri(context) + "attachment/";
+    }
+
+    private static String getCaseDbIndexUri(Context context) {
+        return getCaseDbBaseUri(context) + "index/";
+    }
+
+    public static String getCaseName(Context context, String caseId) {
+        Cursor cursor = getCaseMetaData(context, caseId);
+        if (cursor != null) {
+            cursor.moveToFirst();
+            return cursor.getString(2);
+        }
+        return null;
+    }
+
+    public static String getCaseProperty(Context context, String caseId, String caseProperty) {
+        Cursor caseDataCursor = CaseUtils.getCaseDataCursor(context, caseId);
+        if (caseDataCursor == null) {
+            return null;
+        }
+        while (caseDataCursor.moveToNext()) {
+            String dataKey = caseDataCursor.getString(2);
+            if (caseProperty.equals(dataKey)) {
+                return caseDataCursor.getString(3);
+            }
+        }
+        return null;
+    }
+
+    public static List<String> getCaseIds(Context context) {
+        ArrayList<String> accumulator = new ArrayList<>();
+        Cursor cursor = CaseUtils.getCaseMetaData(context);
+        if (cursor == null) {
+            return null;
+        }
+        while (cursor.moveToNext()) {
+            String caseId = cursor.getString(2);
+            accumulator.add(caseId);
+        }
+        return accumulator;
+    }
+
+    public static Map<String, String> getCaseProperties(Context context, String caseId, ArrayList<String> caseProperties) {
+        HashMap<String, String> propertyMap = new HashMap<>();
+        Cursor caseDataCursor = CaseUtils.getCaseDataCursor(context, caseId);
+        if (caseDataCursor == null) {
+            return null;
+        }
+        while (caseDataCursor.moveToNext()) {
+            String dataKey = caseDataCursor.getString(2);
+            if (caseProperties.contains(dataKey)) {
+                propertyMap.put(dataKey, caseDataCursor.getString(3));
+            }
+        }
+        return propertyMap;
+    }
 
     /**
      * Provides a listing of all cases in the system, along with their metadata.
@@ -37,12 +130,12 @@ public class CaseUtils {
      * @return A cursor over the meta data specified
      */
     public static Cursor getCaseMetaData(Context context, String caseId) {
-        Uri tableUri = Uri.parse(CASE_DB_LIST_URI + caseId);
+        Uri tableUri = Uri.parse(getCaseDbListUri(context) + caseId);
         return context.getContentResolver().query(tableUri, null, null, null, null);
     }
 
     public static Cursor getCaseMetaData(Context context) {
-        Uri tableUri = Uri.parse(CASE_DB_LIST_URI);
+        Uri tableUri = Uri.parse(getCaseDbListUri(context));
         return context.getContentResolver().query(tableUri, null, null, null, null);
     }
 
@@ -61,7 +154,7 @@ public class CaseUtils {
      * @return A cursor over the meta data specified
      */
     public static Cursor getCaseDataCursor(Context context, String caseId) {
-        Uri caseDataUri = Uri.parse(CASE_DB_SINGLE_URI + "data/" + caseId);
+        Uri caseDataUri = Uri.parse(getCaseDbDataUri(context) + caseId);
         return context.getContentResolver().query(caseDataUri, null, null, null, null);
     }
 
@@ -79,7 +172,7 @@ public class CaseUtils {
      value	yes
      */
     public static Cursor getCaseIndexData(Context context, String caseId) {
-        Uri tableUri = Uri.parse(CASE_DB_INDEX_URI + caseId);
+        Uri tableUri = Uri.parse(getCaseDbIndexUri(context) + caseId);
         return context.getContentResolver().query(tableUri, null, null, null, null);
     }
 
@@ -92,7 +185,7 @@ public class CaseUtils {
 
      */
     public static Cursor getCaseAttachmentData(Context context, String caseId) {
-        Uri tableUri = Uri.parse(CASE_DB_ATTACHMENT_URI + caseId);
+        Uri tableUri = Uri.parse(getCaseDbAttachmentUri(context) + caseId);
         return context.getContentResolver().query(tableUri, null, null, null, null);
     }
 }
