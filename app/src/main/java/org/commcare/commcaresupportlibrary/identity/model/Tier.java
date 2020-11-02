a5fda6b85ec04ade216777041a9b240e8d16a306
Shubham Goyal <shubham1g5@gmail.com>
2020-11-02 23:25:45 +0530

    adds verfication model

--- app/src/main/java/org/commcare/commcaresupportlibrary/identity/model/Tier.java
+++ app/src/main/java/org/commcare/commcaresupportlibrary/identity/model/Tier.java
@@ -1,9 +1,14 @@
 package org.commcare.commcaresupportlibrary.identity.model;
 
 public enum Tier {
+    // Almost certain match
     TIER_1,
+    // Very good match
     TIER_2,
+    // Good match
     TIER_3,
+    // Okay match
     TIER_4,
+    // No match
     TIER_5
 }
